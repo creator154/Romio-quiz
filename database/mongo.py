efb778be7967ddd29d7e94ca1cec0635d0694665
creator154 <cteator898@gmail.com>
2026-02-23 19:47:54 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -2,8 +2,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 
 client = AsyncIOMotorClient(MONGO_URI)
-db = client.quizbot
 
-users = db.users
-quizzes = db.quizzes
-games = db.games
+db = client["quizbot"]
+
+quizzes_collection = db["quizzes"]
+games_collection = db["games"]
+users_collection = db["users"]
