a87717301810091332dac0f828d149ad89f2c089
creator154 <cteator898@gmail.com>
2026-02-24 18:02:32 +0530

    Delete utils/states.py

--- utils/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class QuizBuilder(StatesGroup):
-    waiting_title = State()
-    waiting_description = State()
-    waiting_poll = State()
-    editing_title = State()
-    editing_description = State()
