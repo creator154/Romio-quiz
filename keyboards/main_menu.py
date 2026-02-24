2cc9172e6251d10f8bb07e4d360ce9368c8add0e
creator154 <cteator898@gmail.com>
2026-02-24 18:11:54 +0530

    Delete keyboards/main_menu.py

--- keyboards/main_menu.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def main_menu():
-    kb = [
-        [KeyboardButton(text="➕ Create Quiz")],
-        [KeyboardButton(text="📚 View My Quizzes")]
-    ]
-    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
